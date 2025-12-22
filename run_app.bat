854b5403a8f2c6cc434ca85abbf516d5b69ffa0f
TomeMD <tome.maseda@udc.es>
2025-12-22 19:43:50 +0100

    Add scripts to start web service

---
+++ run_app.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: Activar el entorno virtual
+:: call my_venv\Scripts\activate.bat
+
+:: Añadir el directorio src al PYTHONPATH
+set PYTHONPATH=%cd%;%PYTHONPATH%
+
+:: Iniciar la aplicación Streamlit
+streamlit run src\streamlit_app.py
